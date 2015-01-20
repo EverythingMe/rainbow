0521add27e876705f3a73f060754aecd12796cc2
Shalom Yerushalmy <shalom@everything.me>
2015-01-20 09:35:20 +0200

    Added cfn_parameters datasource

--- tests/test_cfn_datasource.py
+++ tests/test_cfn_datasource.py
@@ -39,9 +39,10 @@ class MockCloudformationConnection(object):
 
 
 class MockCloudformationStack(object):
-    def __init__(self, resources={}, outputs={}):
+    def __init__(self, resources={}, outputs={}, parameters={}):
         self._resources = resources
         self._outputs = outputs
+        self._parameters = parameters
 
     def describe_resources(self):
         return [DotDict(logical_resource_id=key, physical_resource_id=value)
@@ -52,6 +53,10 @@ class MockCloudformationStack(object):
         return [DotDict(key=key, value=value)
                 for key, value in self._outputs.iteritems()]
 
+    @property
+    def parameters(self):
+        return [DotDict(key=key, value=value)
+                for key, value in self._parameters.iteritems()]
 
 class TestDataSources(TestCase):
     def setUp(self):
@@ -60,17 +65,21 @@ class TestDataSources(TestCase):
         # c can override b, which can override a
         data_sources = ['cfn_resources:us-stack1',
                         'cfn_outputs:us-stack2',
+                        'cfn_parameters:us-stack3',
                         'cfn_resources:eu-west-1:eu-stack1',
-                        'cfn_outputs:eu-west-1:eu-stack2']
+                        'cfn_outputs:eu-west-1:eu-stack2',
+                        'cfn_parameters:eu-west-1:eu-stack3']
 
         stacks = {
             'us-east-1': {
                 'us-stack1': MockCloudformationStack(resources={'UsResource1': 'us-stack1-UsResource1-ABCDEFGH'}),
-                'us-stack2': MockCloudformationStack(outputs={'UsOutput1': 'US output'})
+                'us-stack2': MockCloudformationStack(outputs={'UsOutput1': 'US output'}),
+                'us-stack3': MockCloudformationStack(parameters={'UsParameter1': 'US parameter'})
             },
             'eu-west-1': {
                 'eu-stack1': MockCloudformationStack(resources={'EuResource1': 'eu-stack1-EuResource1-ABCDEFGH'}),
-                'eu-stack2': MockCloudformationStack(outputs={'EuOutput1': 'EU output'})
+                'eu-stack2': MockCloudformationStack(outputs={'EuOutput1': 'EU output'}),
+                'eu-stack3': MockCloudformationStack(parameters={'EuParameter1': 'EU parameter'})
             }
         }
 
@@ -84,8 +93,14 @@ class TestDataSources(TestCase):
     def test_cfn_outputs_implicit_region(self):
         self.assertEqual(self.datasource_collection.get_parameter_recursive('UsOutput1'), 'US output')
 
+    def test_cfn_parameters_implicit_region(self):
+        self.assertEqual(self.datasource_collection.get_parameter_recursive('UsParameter1'), 'US parameter')
+
     def test_cfn_resources_ecplicit_region(self):
         self.assertEqual(self.datasource_collection.get_parameter_recursive('EuResource1'), 'eu-stack1-EuResource1-ABCDEFGH')
 
     def test_cfn_outputs_explicit_region(self):
         self.assertEqual(self.datasource_collection.get_parameter_recursive('EuOutput1'), 'EU output')
+
+    def test_cfn_parameters_explicit_region(self):
+        self.assertEqual(self.datasource_collection.get_parameter_recursive('EuParameter1'), 'EU parameter')
