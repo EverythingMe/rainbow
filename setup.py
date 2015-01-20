e467eb3c4e28345c5e20ebcfe64db5f384da5b25
Omri Bahumi <omrib@everything.me>
2015-01-20 11:42:54 +0200

    Version bump to 0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rainbow-cfn',
-    version='0.4',
+    version='0.5',
     description='Rainbow is AWS Cloudformation on steroids',
     author='EverythingMe',
     author_email='omrib@everything.me',
